7e31fccc0e9ddfac9300a399f9bf3a88e04dcc34
bdiazc90 <bdiazc90@gmail.com>
2022-12-09 12:30:49 -0500

    adding CORS

--- api2/serializers.py
+++ api2/serializers.py
@@ -1,7 +1,38 @@
 from rest_framework import serializers
 from .models import Task
 
+def starts_with_c(value):
+    if value[0].lower() != 'c':
+        raise serializers.ValidationError("Debe empezar con C")
+
+def must_contains_numbers(value):
+    if not any(char.isdigit() for char in value):
+        raise serializers.ValidationError("Debe contener números")
+
 class TaskSerializador(serializers.ModelSerializer):
+    nombre = serializers.CharField(validators = [starts_with_c, must_contains_numbers])
     class Meta:
         model = Task
         fields = ('nombre', 'prioridad', 'completado_en')
+    
+    # def validate_prioridad(self, value):
+    #     if value == None:
+    #         return value
+    #     if value < 1 or value > 3:
+    #         raise serializers.ValidationError("Prioridad entre 1 y 3")
+    #     return value
+
+    def validate(self, data):
+        prioridad = data.get('prioridad')
+        if prioridad != None and (prioridad < 1 or prioridad > 3):
+            raise serializers.ValidationError("Prioridad entre 1 y 3")
+        # nombre = data.get('nombre')
+        # if contains_number(nombre):
+        #     raise serializers.ValidationError("Nombre no puede tener numeros")
+        return data
+
+def contains_number(string):
+    return any(char.isdigit() for char in string)
+
+# {"nombre": "tarea 6"}
+# {"nombre": "tarea 6", "prioridad": 4}
--- dapi/settings.py
+++ dapi/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'api1',
     'api2',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'dapi.urls'
@@ -124,3 +126,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000'
+]
+    
